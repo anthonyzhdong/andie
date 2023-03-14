21f447e27c07e215c2fc67af377ee63991626796
kerli421 <kerli421@student.otago.ac.nz>
2023-03-14 16:17:00 +1300

    Added filter -> Brighten

--- src/cosc202/andie/Brightness.java
+++ src/cosc202/andie/Brightness.java
@@ -1,5 +1,23 @@
 package cosc202.andie;
+import java.awt.image.*;
 
-public class Brightness {
-    
+public class Brightness implements ImageOperation, java.io.Serializable {
+    Brightness() {
+    // Any construction code goes here
+    }
+    public BufferedImage apply (BufferedImage input) {
+        // The values for the kernel as a 1-element 1 dimensional array
+        float [] array = {1.2f};
+        // Make a 1x1 filter from the array
+        Kernel kernel = new Kernel(1, 1, array);
+        // Apply this as a convolution - same code as in MeanFilter
+        ConvolveOp convOp = new ConvolveOp(kernel);
+        BufferedImage output = new BufferedImage(input.getColorModel(),
+        input.copyData(null),
+        input.isAlphaPremultiplied(), null);
+        convOp.filter(input, output);
+        // And we're done
+        return output;
+        }
+        
 }
