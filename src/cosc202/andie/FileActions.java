850e09aeb54d68cf6ecd129fc2996548e4003d9a
kerli421 <kerli421@student.otago.ac.nz>
2023-03-21 15:33:16 +1300

    Update FileActions.java

--- src/cosc202/andie/FileActions.java
+++ src/cosc202/andie/FileActions.java
@@ -93,7 +93,7 @@ public class FileActions {
          * @param e The event triggering this callback.
          */
         public void actionPerformed(ActionEvent e) {
-            JFileChooser fileChooser = new JFileChooser("");
+            JFileChooser fileChooser = new JFileChooser();
             int result = fileChooser.showOpenDialog(target);
 
             if (result == JFileChooser.APPROVE_OPTION) {
