726e1263306f8646befb230b13687be441c529e0
Anthony Dong <donan928@student.otago.ac.nz>
2023-04-06 14:25:01 +1200

    rotation left complete

--- src/cosc202/andie/RotationLeft.java
+++ src/cosc202/andie/RotationLeft.java
@@ -15,15 +15,19 @@ public class RotationLeft implements ImageOperation, java.io.Serializable  {
         int height = input.getHeight();
 
         BufferedImage rotatedToLeft = new BufferedImage(height,width,input.TYPE_INT_ARGB);
+        //BufferedImage rotatedToLeft = new BufferedImage(width,height,input.TYPE_INT_ARGB);
+
         Graphics2D graphic = rotatedToLeft.createGraphics();
         AffineTransform transform = new AffineTransform();
-        transform.translate(height,0);
-        transform.rotate(Math.PI/2);
+        transform.translate(0,width);
+        transform.rotate(Math.PI/2*3);
+       //transform.translate(width,height);
+      // transform.rotate(Math.PI);
         
         graphic.setTransform(transform);
         graphic.drawImage(input,0,0,null);
         graphic.dispose();
-        
+
         return rotatedToLeft; 
     }
 
