20ee223b31631208feef11a248c0fc210e68b4e7
Anthony Dong <donan928@student.otago.ac.nz>
2023-04-06 14:48:24 +1200

    rotate 180 completed

--- src/cosc202/andie/Rotation180.java
+++ src/cosc202/andie/Rotation180.java
@@ -4,9 +4,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.geom.AffineTransform;
 
-public class RotationLeft implements ImageOperation, java.io.Serializable  {
+public class Rotation180 implements ImageOperation, java.io.Serializable  {
 
-    RotationLeft(){
+    Rotation180(){
         
     
     }
@@ -14,17 +14,19 @@ public class RotationLeft implements ImageOperation, java.io.Serializable  {
         int width = input.getWidth();
         int height = input.getHeight();
 
-        BufferedImage rotatedToLeft = new BufferedImage(height,width,input.TYPE_INT_ARGB);
-        Graphics2D graphic = rotatedToLeft.createGraphics();
+        
+        BufferedImage rotated180 = new BufferedImage(width,height,input.TYPE_INT_ARGB);
+
+        Graphics2D graphic = rotated180.createGraphics();
         AffineTransform transform = new AffineTransform();
-        transform.translate(0,width);
-        transform.rotate(Math.PI/2*3);
-          
+        transform.translate(width,height);
+        transform.rotate(Math.PI);
+        
         graphic.setTransform(transform);
         graphic.drawImage(input,0,0,null);
         graphic.dispose();
 
-        return rotatedToLeft; 
+        return rotated180; 
     }
 
 }
